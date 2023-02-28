68c9594bf7c1a47145ad8511b80521ad5625e9f5
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-28 19:41:51 -0300

    build: initial files

---
+++ lib/Rspec_portuguese_course.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Rspec_portuguese_course/version"
+
+module RspecPortugueseCourse
+  class Error < StandardError; end
+  # Your code goes here...
+end
