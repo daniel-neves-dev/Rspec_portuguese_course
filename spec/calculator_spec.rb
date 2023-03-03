48db6b4f5bd6e7f49869fbbbb8fba7b0baff6816
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-03 20:11:52 -0300

    refactor: calculator_spec code

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,24 +3,18 @@
 require "calculator"
 
 RSpec.describe Calculator do
-  context "sum two positive numbers" do
-    it "#sum" do
-      calc = Calculator.new
-      expect(calc.sum(1,2)).to eq(3)
-    end
-  end
 
-  context "sum tow numbers with one being negative " do
-    it "#sum" do
-      calc = Calculator.new
-      expect(calc.sum(-7,5)).to eq(-2)
+  context "#sum method" do
+    it "sum two positive numbers" do
+      expect(subject.sum(1,2)).to eq(3)
     end
-  end
 
-  context "sum two negative numbers" do
-    it "#sum" do
-      calc = Calculator.new
-      expect(calc.sum(-2, -4)).to eq(-6)
+    it "sum tow numbers with one being negative " do
+      expect(subject.sum(-7,5)).to eq(-2)
+    end
+
+    it "sum two negative numbers" do
+      expect(subject.sum(-2, -4)).to eq(-6)
     end
   end
 end
