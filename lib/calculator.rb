a277704211eeab773cd4ca390992ff7989d5fe6b
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-03 19:34:53 -0300

    test: calculator, with positive and negative numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class Calculator
   def sum(n1, n2)
     n1 + n2
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Calculator do
       expect(calc.sum(1,2)).to eq(3)
     end
   end
+
+  context "sum tow numbers with one being negative " do
+    it "#sum" do
+      calc = Calculator.new
+      expect(calc.sum(-7,5)).to eq(-2)
+    end
+  end
+
+  context "sum two negative numbers" do
+    it "#sum" do
+      calc = Calculator.new
+      expect(calc.sum(-2, -4)).to eq(-6)
+    end
+  end
 end
