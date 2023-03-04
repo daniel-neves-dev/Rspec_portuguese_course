9e19d65ec6f8c0ebcfb792fdd7f4e1dc3b380148
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-04 11:19:47 -0300

    test: implicit subject

---
+++ spec/implicit_subject_spec.rb
@@ -0,0 +1,9 @@
+require "implicit_subject"
+
+RSpec.describe String do
+  describe NotEmptyString do
+    it "not empty" do
+      expect(subject).to eq("This is string is not empty")
+    end
+  end
+end
