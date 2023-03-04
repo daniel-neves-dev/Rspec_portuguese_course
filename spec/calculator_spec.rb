f33c1f6e7b1aa6cd622a3f76dc13873de29d60d9
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-04 11:52:13 -0300

    test: implicit subject

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,14 +3,13 @@
 require "calculator"
 
 RSpec.describe Calculator do
-
   context "#sum method" do
     it "sum two positive numbers" do
-      expect(subject.sum(1,2)).to eq(3)
+      expect(subject.sum(1, 2)).to eq(3)
     end
 
     it "sum tow numbers with one being negative " do
-      expect(subject.sum(-7,5)).to eq(-2)
+      expect(subject.sum(-7, 5)).to eq(-2)
     end
 
     it "sum two negative numbers" do
