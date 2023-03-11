7fd7f9b90b990f77fd145dbf3fa4ccfc98238b40
daniel <daniel.oliveira.au@hotmail.com>
2023-03-11 20:27:16 -0300

    text: collection matchers

---
+++ lib/collection_matchers.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class TestAll
+  def numbers
+    [1, 3, 5]
+  end
+  def words
+    %w[Ruby Rails]
+  end
+end
--- spec/collection_matchers_spec.rb
+++ spec/collection_matchers_spec.rb
@@ -1,1 +1,8 @@
+# frozen_string_literal: true
+
 require "collection_matchers"
+
+RSpec.describe TestAll do
+  it { expect(subject.numbers).to all(be_odd.and(be_an(Integer))) }
+  it { expect(subject.words).to all((be_a(String).and include('R'))) }
+end
